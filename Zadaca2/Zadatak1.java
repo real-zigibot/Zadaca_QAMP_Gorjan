b0bf1d0599f583dc3b5b314dfaaeb2a06f9df8b1
unknown <slagalo39@gmail.com>
2022-02-16 22:18:22 +0100

    Dodaci zadatku 1

--- Zadaca2/Zadatak1.java
+++ Zadaca2/Zadatak1.java
@@ -5,15 +5,29 @@ public class Zadatak1 {
         int x=0;
         Input(x);
     }
+    public static void Output(int[] InputArray,int size){
+        for(int i=0;i<size;++i){
+            if(InputArray[i]%2==0){
+                System.out.format("%d. element je %d. Paran je ",i+1,InputArray[i]);
+            }
+            else
+                System.out.format("%d. element je %d. Nije paran",i+1,InputArray[i]);
+            if(InputArray[i]>10)
+                System.out.print(" i veci je od 10.\n");
+            else
+                System.out.print(" i nije veci od 10.\n");
+        }
+    }
     public static void Input(int flag){
         System.out.print("Unesi velicinu niza(cijeli broj): ");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] InputArray=new int[size];
-        for(int i=0;i< InputArray.length;++i) {
+        for(int i=0;i<InputArray.length;++i) {
             System.out.format("Unesi %d. element niza(cijeli broj): ", i + 1);
             InputArray[i] = sc.nextInt();
         }
+        Output(InputArray,size);
         System.out.print("Sadrzaj niza je sljedeci: "+ Arrays.toString(InputArray));
     }
 
